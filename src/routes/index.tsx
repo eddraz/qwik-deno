d1b10b442025d9a261dd9b8b0c87c043ebdda6f1
edd.ramirez@proton.me <edd.ramirez@proton.me>
2024-09-11 08:34:49 -0500

    Add Qwik with DENO

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,43 @@
-import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import { component$, useSignal } from "@builder.io/qwik";
+import { Form, routeAction$, type DocumentHead } from "@builder.io/qwik-city";
+
+export const useAddUser = routeAction$(async (data, request) => {
+  request.cookie.set("id", "data.id");
+
+  return {
+    success: true,
+    data,
+  };
+});
 
 export default component$(() => {
+  const action = useAddUser();
+  const id = useSignal<string>(Date.now().toString());
+
   return (
     <>
-      <h1>Hi 👋</h1>
-      <div>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </div>
+      {action.value?.success && <>{JSON.stringify(action.value.data)}</>}
+
+      <Form action={action}>
+        <input
+          type="text"
+          name="id"
+          class="border-2"
+          placeholder="id"
+          bind:value={id}
+        />
+        <input
+          type="text"
+          name="displayName"
+          class="border-2"
+          placeholder="nombre"
+        />
+        <input type="text" name="email" class="border-2" placeholder="email" />
+
+        <button type="submit" class="mx-2 rounded-md bg-green-300">
+          Submit!
+        </button>
+      </Form>
     </>
   );
 });
